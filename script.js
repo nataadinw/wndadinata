e113c9a6cf176ae75fb73bf112582b25434580dc
nataadinw <wandadinata13@gmail.com>
2024-10-19 15:11:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var div = document.getElementById('main');
+var display = 0;
+
+function hideShow()
+{
+    if (display == 1);
+    {
+        div.style.display = 'block';
+        display = 0;
+    }
+    else
+    {
+        div.style.display ='none';
+        display = 1;
+    }
+}
